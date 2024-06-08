993f6845299a6543702ec5feddad878226cd69c9
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-06-08 12:35:39 +1000

    updated main

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"net/http"
 
-	"github.com/wickkan/Financial-Data-Visualisation/tree/main/handlers"
+	"github.com/wickkan/financial-data-visualisation/handlers"
+	"github.com/wickkan/financial-data-visualisation/websockets"
 )
 
 func main() {
 	http.HandleFunc("/", handlers.HomeHandler)
 	http.HandleFunc("/api/realtime", handlers.RealTimeDataHandler)
 	http.HandleFunc("/api/historical", handlers.HistoricalDataHandler)
+	http.HandleFunc("/ws", websockets.WebSocketHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
